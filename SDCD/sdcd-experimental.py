9fc831cad7c818e5644694579df76422bccc8ffb
jafar-albadarneh <jafar.albadarneh@gmail.com>
2017-12-19 00:41:41 +0200

    add 2 wlan onterfaces to APs

--- SDCD/sdcd-experimental.py
+++ SDCD/sdcd-experimental.py
@@ -65,18 +65,18 @@ def topology():
         cars[x] = net.addCar('car%s' % (x), wlans=1,
                              ip='10.0.0.%s/8' % (x + 1), encrypt='wpa2', cls=car_type)
 
-    e1 = net.addAccessPoint('e1', ssid='vanet-ssid', mac='00:00:00:11:00:01', mode='g', channel='1',
-                            passwd='123456789a', encrypt='wpa2', position='3332.62,3253.92,0', cls=eNodeB, inNamespace=True)
-    e2 = net.addAccessPoint('e2', ssid='vanet-ssid', mac='00:00:00:11:00:02', mode='g', channel='1',
-                            passwd='123456789a', encrypt='wpa2', position='3279.02,3736.27,0', cls=eNodeB, inNamespace=True)
-    e3 = net.addAccessPoint('e3', ssid='vanet-ssid', mac='00:00:00:11:00:03', mode='g', channel='11',
-                            passwd='123456789a', encrypt='wpa2', position='2806.42,3395.22,0', cls=eNodeB, inNamespace=True)
-    e4 = net.addAccessPoint('e4', ssid='vanet-ssid', mac='00:00:00:11:00:04', mode='g', channel='6',
-                            passwd='123456789a', encrypt='wpa2', position='2320.82,3565.75,0', cls=eNodeB, inNamespace=True)
-    e5 = net.addAccessPoint('e5', ssid='vanet-ssid', mac='00:00:00:11:00:05', mode='g', channel='6',
-                            passwd='123456789a', encrypt='wpa2', position='2887.62,2935.61,0', cls=eNodeB, inNamespace=True)
-    e6 = net.addAccessPoint('e6', ssid='vanet-ssid', mac='00:00:00:11:00:06', mode='g', channel='11',
-                            passwd='123456789a', encrypt='wpa2', position='2351.68,3083.40,0', cls=eNodeB, inNamespace=True)
+    e1 = net.addAccessPoint('e1', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:01', mode='g', channel='1',
+                            passwd='123456789a', encrypt='wpa2', position='3332.62,3253.92,0', cls=eNodeB)
+    e2 = net.addAccessPoint('e2', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:02', mode='g', channel='1',
+                            passwd='123456789a', encrypt='wpa2', position='3279.02,3736.27,0', cls=eNodeB)
+    e3 = net.addAccessPoint('e3', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:03', mode='g', channel='11',
+                            passwd='123456789a', encrypt='wpa2', position='2806.42,3395.22,0', cls=eNodeB)
+    e4 = net.addAccessPoint('e4', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:04', mode='g', channel='6',
+                            passwd='123456789a', encrypt='wpa2', position='2320.82,3565.75,0', cls=eNodeB)
+    e5 = net.addAccessPoint('e5', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:05', mode='g', channel='6',
+                            passwd='123456789a', encrypt='wpa2', position='2887.62,2935.61,0', cls=eNodeB)
+    e6 = net.addAccessPoint('e6', wlans=2, ssid='vanet-ssid', mac='00:00:00:11:00:06', mode='g', channel='11',
+                            passwd='123456789a', encrypt='wpa2', position='2351.68,3083.40,0', cls=eNodeB)
 
     client = net.addHost('cloud', cls=Cloud_host)
     switch = net.addSwitch('switch', dpid='4000000000000000', cls=SD_Switch)
